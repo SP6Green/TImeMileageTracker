9223efa37180aa7049eb892b8f0a54a829f390a5
SP6Green <sp6green@gmail.com>
2024-10-05 00:04:13 -0400

    test

--- App.js
+++ App.js
@@ -1,14 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let loc = await Location.getCurrentPositionAsync({});
+      setLocation(loc.coords);
+
+      Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 5000, 
+          distanceInterval: 10, 
+        },
+        (newLocation) => {
+          setLocation(newLocation.coords);
+        }
+      );
+    })();
+  }, []);
+
+  if(!location) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={StyleSheet.paragraph}>
+          {errorMsg ? errorMsg : 'Fetching location...'}
+        </Text>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        showsUserLocation={true}
+        followsUserLocation={true}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="You are here"
+          />
+        </MapView>
+        <View style={styles.coordinatesContainer}>
+          <Text style={styles.coordinatesText}>
+            Latitude: {location.latitude}, Longitude: {location.longitude}
+          </Text>
+        </View>
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +80,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+  coordinatesContainer: {
+    position: 'absolute',
+    bottom: 30,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    padding: 10,
+    borderRadius: 10,
+  },
+  coordinatesText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
+
 });
